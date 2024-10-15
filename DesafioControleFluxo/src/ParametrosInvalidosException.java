e2c2fd876e76bb8e72dd9afeaa23ed15e6db7b67
zzmt7 <geral.brrs@gmail.com>
2024-10-15 15:08:49 -0300

    commit 2 desaifo DIO - Controle de Fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException() {
+        System.out.println("O parâmentro um deve ser maior que o parâmetro dois!");;
+    }
+}
